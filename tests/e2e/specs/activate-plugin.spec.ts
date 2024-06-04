af6ad3d556dbc8881d1a64ca425330b95834635d
Sadi Mohammad Zaman <sadi@coderex.co>
2024-06-04 09:41:45 +0600

    update e2e yml file

--- tests/e2e/specs/activate-plugin.spec.ts
+++ tests/e2e/specs/activate-plugin.spec.ts
@@ -3,21 +3,24 @@
  */
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-const { activatePlugin, page } = require('@wordpress/e2e-test-utils');
-
-
 test.describe( 'Plugin Activation', () => {
-	test.beforeAll( async ( { requestUtils } ) => {
-		await requestUtils.activatePlugin(
-			'continuous-integrations'
-		);
+	test( 'should activate the plugin and validate its dashboard', async ( { admin, page } ) => {
+		try {
+			await admin.visitAdminPage( 'admin.php?page=continuous-integrations#/' );
+			await expect(page.getByText('Welcome to React boilerplate for WordPress plugin\n')).toBeVisible();
+		} catch (error) {
+			console.error('Error during test execution:', error);
+			throw error; // Re-throw the error to fail the test
+		}
 	} );
 
-	test( 'should activate the plugin and validate its dashboard', async ( { admin, page } ) => {
-		// Activate the plugin
-		await admin.visitAdminPage( 'admin.php?page=continuous-integrations#/' );
-
-		await expect(page.getByText('Welcome to React boilerplate for WordPress plugin\n')).toBeVisible();
-
+	test( 'Failed test', async ( { admin, page } ) => {
+		try {
+			await admin.visitAdminPage( 'admin.php?page=continuous-integrations#/' );
+			await expect(page.getByText('Welcome to Vue')).toBeVisible();
+		} catch (error) {
+			console.error('Error during test execution:', error);
+			throw error; // Re-throw the error to fail the test
+		}
 	} );
 } );
